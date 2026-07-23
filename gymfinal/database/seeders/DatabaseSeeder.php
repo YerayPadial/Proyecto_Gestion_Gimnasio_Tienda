dbde019100763ba4515d39a0d1e7b9f92ccd02e1
ypadial <ypadial@stemdo.io>
2026-07-23 10:05:10 +0200

    Mejora tienda FinalGym y productos con imagen

--- gymfinal/database/seeders/DatabaseSeeder.php
+++ gymfinal/database/seeders/DatabaseSeeder.php
@@ -46,38 +46,68 @@ class DatabaseSeeder extends Seeder
             ['descripcion' => 'Productos de apoyo para el entrenamiento.']
         );
 
-        Inventario::updateOrCreate(
-            ['nombre' => 'Camiseta FinalGym'],
+        $accesorios = Categorias::updateOrCreate(
+            ['tipo' => 'Accesorios'],
+            ['descripcion' => 'Complementos para mejorar cada sesión de entrenamiento.']
+        );
+
+        $products = [
             [
+                'nombre' => 'Camiseta FinalGym',
                 'tipo_categoria' => $ropa->id,
                 'precio' => 19.90,
                 'existencias' => 18,
                 'descripcion' => 'Camiseta tecnica transpirable para entrenamiento.',
-                'foto' => null,
-            ]
-        );
-
-        Inventario::updateOrCreate(
-            ['nombre' => 'Shaker deportivo'],
+                'foto' => 'products/finalgym-camiseta.svg',
+            ],
             [
+                'nombre' => 'Shaker deportivo',
                 'tipo_categoria' => $suplementos->id,
                 'precio' => 8.50,
                 'existencias' => 25,
-                'descripcion' => 'Botella mezcladora para batidos y bebidas isotónicas.',
-                'foto' => null,
-            ]
-        );
-
-        Inventario::updateOrCreate(
-            ['nombre' => 'Guantes de entrenamiento'],
+                'descripcion' => 'Botella mezcladora para batidos y bebidas isotonicas.',
+                'foto' => 'products/finalgym-shaker.svg',
+            ],
             [
+                'nombre' => 'Guantes de entrenamiento',
                 'tipo_categoria' => $ropa->id,
                 'precio' => 14.90,
                 'existencias' => 10,
                 'descripcion' => 'Guantes con agarre para ejercicios de fuerza.',
-                'foto' => null,
-            ]
-        );
+                'foto' => 'products/finalgym-guantes.svg',
+            ],
+            [
+                'nombre' => 'Toalla premium FinalGym',
+                'tipo_categoria' => $accesorios->id,
+                'precio' => 12.90,
+                'existencias' => 20,
+                'descripcion' => 'Toalla compacta de microfibra para sala y vestuario.',
+                'foto' => 'products/finalgym-toalla.svg',
+            ],
+            [
+                'nombre' => 'Proteina Whey Pro',
+                'tipo_categoria' => $suplementos->id,
+                'precio' => 34.90,
+                'existencias' => 12,
+                'descripcion' => 'Proteina sabor vainilla para recuperacion muscular.',
+                'foto' => 'products/finalgym-proteina.svg',
+            ],
+            [
+                'nombre' => 'Banda elastica resistencia',
+                'tipo_categoria' => $accesorios->id,
+                'precio' => 9.90,
+                'existencias' => 30,
+                'descripcion' => 'Banda de resistencia media para movilidad y fuerza.',
+                'foto' => 'products/finalgym-banda.svg',
+            ],
+        ];
+
+        foreach ($products as $product) {
+            Inventario::updateOrCreate(
+                ['nombre' => $product['nombre']],
+                $product
+            );
+        }
 
         Clientes::updateOrCreate(
             ['dni' => '12345678A'],
